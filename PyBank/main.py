be2dfd28c352f8de08ebafdd841989da6a7c84a0
Timothy O'Malley <tomalley@landmarkeducation.com>
2020-08-15 09:50:28 -0500

    final answer

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,67 @@
-print("hello")
+#import budget data 
+import os
+import csv 
+#working directory
+
+csvpath=os.path.join('Resources','budget_data.csv')
+# *** Note this following section I was not able to solve.  I am including the solution as to complete the rest of the project but should not get credit.
+# #     csvreader = csv.reader(csvfile, delimiter=',')
+#     print(csvreader)
+#     csv_header = next(csvreader)
+#     month = []
+#     revenue = []
+#     revenue_change = []
+#     monthly_change = []
+    
+#     print(f"Header: {csv_header}")               
+
+# #Months       
+#     for row in csvreader:
+#         month.append(row[0])
+#         revenue.append(row[1])
+#     print(len(month))
+#  #Revenue 
+#     revenue_int = map(int,revenue)
+#     total_revenue = (sum(revenue_int))
+#     print(total_revenue)
+
+ #Avg Change
+    i = 0
+    for i in range(len(revenue) - 1):
+        profit_loss = int(revenue[i+1]) - int(revenue[i])
+ # append profit_loss
+        revenue_change.append(profit_loss)
+    Total = sum(revenue_change)
+    #print(revenue_change)
+    monthly_change = Total / len(revenue_change)
+    print(monthly_change)
+    #print(Total)
+    
+#Greatest Increase
+    profit_increase = max(revenue_change)
+    print(profit_increase)
+    k = revenue_change.index(profit_increase)
+    month_increase = month[k+1]
+    
+#Greatest Decrease
+    profit_decrease = min(revenue_change)
+    print(profit_decrease)
+    j = revenue_change.index(profit_decrease)
+    month_decrease = month[j+1]
+
+
+#Print Statements
+
+print(f'Financial Analysis'+'\n')
+print(f'----------------------------'+'\n')
+
+
+print("Total number of months: " + str(len(month)))
+
+print("Total Revenue in period: $ " + str(total_revenue))
+      
+print("Average monthly change in Revenue : $" + str(monthly_change))
+
+print(f"Greatest Increase in Profits: {month_increase} (${profit_increase})")
+
+print(f"Greatest Decrease in Profits: {month_decrease} (${profit_decrease})")
